2917ad8d4385181c8b799a1053cb35a8a61fe766
Alex Roh <alex.roh@nyu.edu>
2018-11-02 14:21:30 -0700

    set up locationlistener and manager

--- app/src/main/java/com/digitalidllc/alex_roh/memorableplaces/MainActivity.java
+++ app/src/main/java/com/digitalidllc/alex_roh/memorableplaces/MainActivity.java
@@ -1,13 +1,84 @@
 package com.digitalidllc.alex_roh.memorableplaces;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView placesLV;
+    private ArrayList<favoritePlace> placesList;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    class favoritePlace{
+        private Location mPlace;
+        private String mAddress;
+
+        @Override
+        public String toString(){
+            return mAddress;
+        }
+
+        public favoritePlace(Location place, String address){
+            this.mPlace=place;
+            this.mAddress=address;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("location changed to", location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+    }
+
+    private void setUpListView(){
+        placesList=new ArrayList<>();
+        //add current location as first element
+        
+
+        placesLV = findViewById(R.id.placesLV);
+
+
     }
 }
